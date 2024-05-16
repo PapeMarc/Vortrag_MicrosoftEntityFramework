d7b8609f47d7cbcfc3a147730a1014378d302fee
Marc Pape <marc.pape@pape-development.com>
2024-05-16 17:07:48 +0200

    reworked example a bit

--- Program.cs
+++ Program.cs
@@ -1,28 +1,32 @@
 ﻿using MicrosoftEntityFramework.Data;
+using MicrosoftEntityFramework.Model;
 
-AppDbContext database = null;
+AppDbContext? database = null;
 
 try
 {
     database = new AppDbContext();
 
-    //database.User.Add(new MicrosoftEntityFramework.Model.User { EMail = "marc.pape@edu.fhdw.de", Name = "Marc" });
-    //database.User.Add(new MicrosoftEntityFramework.Model.User { EMail = "", Name = "" });
-    //database.User.Remove(database.User.OrderBy(a => a.UserID).Last());
-    //database.SaveChanges();
+    // database.User.Add(new MicrosoftEntityFramework.Model.User { EMail = "", Name = "" });
+    database.User.Add(new User { EMail = "Timo.Behr@edu.fhdw.de", Name = "Timo Behr" });
+    database.User.Add(new User { EMail = "Moritz.Wehlitz@edu.fhdw.de", Name = "Moritz Wehlitz" });
 
-    //database.User.Add(new MicrosoftEntityFramework.Model.User { EMail = "Jonas.Knewitz@edu.bib.de", Name = "Jonas Knewitz", UserID=3 });
-    //database.User.Remove(database.User.OrderBy(a => a.UserID).Last());
-    //database.SaveChanges();
+    // database.User.OrderBy(predicate);
+    User lastAddedUser = database.User.OrderBy(a => a.UserID).Last();
 
-    var users = database.User.ToList();
+    // database.User.Remove(User);
+    database.User.Remove(lastAddedUser);
 
+    database.SaveChanges();
+
+    // Ausgabe aller Benutzer in der Konsole
+    List<User> users = database.User.ToList();
     foreach (var user in users)
     {
         Console.WriteLine(
+            user.UserID + ": " +
             user.Name + ", " +
-            user.EMail + ", " +
-            user.UserID + '.'
+            user.EMail + "."
         );
     }
     Console.ReadLine();
